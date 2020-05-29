2154902733e53b48fa75a93750cc387c2988a371
Hm <Manjunath_Hm@Dellteam.com>
2020-05-29 12:03:03 +0530

    #1. Initial Commit

---
+++ src/main/java/org/manjunath/springboot/applicationwardemo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.manjunath.springboot.applicationwardemo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApplicationWarDemoApplication.class);
+	}
+
+}
---
+++ src/test/java/org/manjunath/springboot/applicationwardemo/ApplicationWarDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.manjunath.springboot.applicationwardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationWarDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
